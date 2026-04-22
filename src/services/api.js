d1ffd6135d2049a9f747623adbe732eabc3d3ac2
clebermarcolino <clebermarcolinogba@gmail.com>
2026-04-22 12:59:27 -0300

    feat: responsividade na tela de login

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    //baseURL: "https://kandev-production-d837.up.railway.app",
-    baseURL: "http://localhost:3001",
+    baseURL: "https://kandev-production-d837.up.railway.app",
+    //baseURL: "http://localhost:3001",
 });
 
 export default api;
