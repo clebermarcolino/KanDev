005ab3c1f9717d5d28733a193d9cdf443c9b9369
clebermarcolino <clebermarcolinogba@gmail.com>
2026-04-21 22:40:53 -0300

    fix: server.js usando ES modules

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+import jsonServer from 'json-server'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
 
-server.use(middlewares);
-server.use(router);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
-server.listen(process.env.PORT || 3001, () => {
-  console.log('JSON Server rodando!');
-});
+const server = jsonServer.create()
+const router = jsonServer.router(join(__dirname, 'db.json'))
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+
+const PORT = process.env.PORT || 3001
+server.listen(PORT, () => {
+  console.log(`JSON Server rodando na porta ${PORT}`)
+})
