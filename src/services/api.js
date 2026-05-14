3b3e2f2bd5c805925faaa3211f28b79dec0647c9
clebermarcolino <clebermarcolinogba@gmail.com>
2026-05-14 17:59:01 -0300

    style: arredondamento de botão para adicionar tarefana versão mobile

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://kandev-production-d837.up.railway.app",
+    baseURL: "https://kandev-production-d837.up.railway.app"
 });
 
 export default api;
